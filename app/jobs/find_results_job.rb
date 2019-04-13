38d2d7aa859048f7ef9e569eaa4e998230f02b70
Ritwick <malhotraritwick2011@gmail.com>
2019-04-13 06:46:06 +0200

    asd'

--- app/jobs/find_results_job.rb
+++ app/jobs/find_results_job.rb
@@ -47,41 +47,41 @@ class FindResultsJob < ApplicationJob
 			end 
 		end
 
-		if added < 10
-			allegro_similar = URI.parse("https://api.allegro.pl/offers/listing?phrase=#{barcode.code}&limit=10&74019")
-			http_allegro_similar = Net::HTTP.new(allegro_similar.host, allegro_similar.port)
-			http_allegro_similar.use_ssl = true
-			headers = {
-				'Authorization' => "Bearer #{result['access_token']}",
-				'Accept' => 'application/vnd.allegro.public.v1+json'
-			}
-			req_allegro_similar = Net::HTTP::Get.new(allegro_similar, headers)
-			res_allegro_similar = http_allegro.request(req_allegro_similar)
-			result_allegro_similar = JSON.parse(res_allegro_similar.body)
+		# if added < 10
+		# 	allegro_similar = URI.parse("https://api.allegro.pl/offers/listing?phrase=#{barcode.code}&limit=10&")
+		# 	http_allegro_similar = Net::HTTP.new(allegro_similar.host, allegro_similar.port)
+		# 	http_allegro_similar.use_ssl = true
+		# 	headers = {
+		# 		'Authorization' => "Bearer #{result['access_token']}",
+		# 		'Accept' => 'application/vnd.allegro.public.v1+json'
+		# 	}
+		# 	req_allegro_similar = Net::HTTP::Get.new(allegro_similar, headers)
+		# 	res_allegro_similar = http_allegro.request(req_allegro_similar)
+		# 	result_allegro_similar = JSON.parse(res_allegro_similar.body)
 
-			if result_allegro['items']['promoted'].count > 2
-				result_allegro['items']['promoted'].each do |item|
-					image = item['images'].first['url'] if item['images'].first['url']
-					Result.create(
-						name: item['name'], 
-						image: image,
-						price: item['delivery']['lowestPrice']['amount'],
-						barcode: barcode
-					)
-				end
-			else
-				result_allegro['items']['regular'].each do |item|
-					result_allegro['items']['regular'].each do |item|
-						image = item['images'].first['url'] if item['images'].first['url']
-						Result.create(
-							name: item['name'], 
-							image: image,
-							price: item['delivery']['lowestPrice']['amount'],
-							barcode: barcode
-						)
-					end
-				end 
-			end
-		end
+		# 	if result_allegro['items']['promoted'].count > 2
+		# 		result_allegro['items']['promoted'].each do |item|
+		# 			image = item['images'].first['url'] if item['images'].first['url']
+		# 			Result.create(
+		# 				name: item['name'], 
+		# 				image: image,
+		# 				price: item['delivery']['lowestPrice']['amount'],
+		# 				barcode: barcode
+		# 			)
+		# 		end
+		# 	else
+		# 		result_allegro['items']['regular'].each do |item|
+		# 			result_allegro['items']['regular'].each do |item|
+		# 				image = item['images'].first['url'] if item['images'].first['url']
+		# 				Result.create(
+		# 					name: item['name'], 
+		# 					image: image,
+		# 					price: item['delivery']['lowestPrice']['amount'],
+		# 					barcode: barcode
+		# 				)
+		# 			end
+		# 		end 
+		# 	end
+		# end
 	end
 end
